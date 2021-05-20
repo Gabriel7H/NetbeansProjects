01e0f2b0bf46667a887fe250ecf245302add0205
Gabriel7H <gabrielamorim338@outlook.com>
2021-05-20 16:01:04 -0300

    modifiquei a variavel x

--- Comissão 2/src/principal2/Principal2.java
+++ Comissão 2/src/principal2/Principal2.java
@@ -6,7 +6,8 @@ public class Principal2 {
     public static void main(String[] args) {
         Vendedor1 v1 = new Vendedor1();
         
-        int x = 2;
+        int z = 2;
+        
     }
     
 }
